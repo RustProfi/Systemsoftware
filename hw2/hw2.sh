a5b4e95344e3dfa5a914ab84e63664ffa584efc9
Marno Janetzky <janetzkymarno@gmail.com>
2018-11-07 17:58:15 +0100

    script update

--- hw2/hw2.sh
+++ hw2/hw2.sh
@@ -5,40 +5,50 @@ build-artifacts()
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
 
-#wget "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.11.tar.xz"
-#tar xf linux-4.11.tar.xz
-#wget "http://busybox.net/downloads/busybox-1.26.2.tar.bz2"
-#tar xvjf busybox-1.26.2.tar.bz2
-#wget "https://matt.ucc.asn.au/dropbear/releases/dropbear-2016.74.tar.bz2"
-#tar xvjf dropbear-2016.74.tar.bz2
+#Download
+wget "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.11.tar.xz"
+tar xf linux-4.11.tar.xz
+wget "http://busybox.net/downloads/busybox-1.26.2.tar.bz2"
+tar xvjf busybox-1.26.2.tar.bz2
+wget "https://matt.ucc.asn.au/dropbear/releases/dropbear-2016.74.tar.bz2"
+tar xvjf dropbear-2016.74.tar.bz2
 
-#cp kernel/.config linux-4.11
-#cp busybox/.config busybox-1.26.2
-#cp dropbear/options.h dropbear-2016.74
+#Copy configs
+cp kernel/.config linux-4.11
+cp busybox/.config busybox-1.26.2
+cp dropbear/options.h dropbear-2016.74
 
 mkdir artifacts
 
+#build
 cd linux-4.11
-#make -j5
+make -j5
 cp arch/x86_64/boot/bzImage ../artifacts
 cd ../busybox-1.26.2
-#make -j5
+make -j5
 cp busybox ../initrd/bin
 
 cd ../dropbear-2016.74
-#./configure --disable-shadow --disable-lastlog --disable-syslog --disable-wtmp --disable-wtmpx --disable-utmpx
+./configure --disable-shadow --disable-lastlog --disable-syslog --disable-wtmp --disable-wtmpx --disable-utmpx
 sed -i 's/22/22222/g' options.h
-#make STATIC=1 MULTI=1
+make STATIC=1 MULTI=1
 cp dropbearmulti ../initrd/bin
+cp dropbearmulti ../artifacts
 
 cd ../sysinfo
 make
 cp sysinfo ../initrd/bin
 
 cd ../initrd
+
+#copy shared libs
+mkdir lib
+cp ../../../../../../lib/x86_64-linux-gnu/ld-linux-x86-64.so.2 lib
+cp ../../../../../../lib/x86_64-linux-gnu/libc.so.6 lib
+cp ../../../../../../lib/x86_64-linux-gnu/libnss_files.so.2 lib 
+ 
+#create cpio
 find | cpio -L -v -o -H newc > ../artifacts/initrd.cpio
-
-
 }
 
 clean()
@@ -52,6 +62,9 @@ rm  linux-4.11.tar.xz
 rm  busybox-1.26.2.tar.bz2
 rm -r dropbear-2016.74
 rm dropbear-2016.74.tar.bz2
+
+rm -r initrd/lib
+rm -r artifacts
 }
 
 usage()
