408d1e6dfc85b6dc3c1579c599d7b0e1b2963621
Marno Janetzky <janetzkymarno@gmail.com>
2018-11-07 14:37:15 +0100

    hw2.sh dropbear and QNA one question

--- hw2/hw2.sh
+++ hw2/hw2.sh
@@ -9,15 +9,24 @@ wget "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.11.tar.xz"
 tar xf linux-4.11.tar.xz
 wget "http://busybox.net/downloads/busybox-1.26.2.tar.bz2"
 tar xvjf busybox-1.26.2.tar.bz2
+wget "https://matt.ucc.asn.au/dropbear/releases/dropbear-2016.74.tar.bz2"
+tar xvjf dropbear-2016.74.tar.bz2
 
 cp kernel/.config linux-4.11
 cp busybox/.config busybox-1.26.2
+cp dropbear/options.h dropbear-2016.74
 
 cd linux-4.11
 make -j5
 
 cd ../busybox-1.26.2
 make -j5
+
+cd ../dropbear-2016.74
+./configure --disable-shadow --disable-lastlog --disable-syslog --disable-wtmp --disable-wtmpx --disable-utmpx
+make STATIC=1
+
+
 }
 
 clean()
