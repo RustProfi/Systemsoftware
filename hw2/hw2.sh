58d5e7c6b92719d73173b1fd825b40b7d392f192
Marno Janetzky <janetzkymarno@gmail.com>
2018-11-07 17:22:21 +0100

    update hw2.sh

--- hw2/hw2.sh
+++ hw2/hw2.sh
@@ -5,30 +5,35 @@ build-artifacts()
 BASEDIR=$(dirname "$0")
 cd $BASEDIR
 
-wget "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.11.tar.xz"
-tar xf linux-4.11.tar.xz
-wget "http://busybox.net/downloads/busybox-1.26.2.tar.bz2"
-tar xvjf busybox-1.26.2.tar.bz2
-wget "https://matt.ucc.asn.au/dropbear/releases/dropbear-2016.74.tar.bz2"
-tar xvjf dropbear-2016.74.tar.bz2
+#wget "https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.11.tar.xz"
+#tar xf linux-4.11.tar.xz
+#wget "http://busybox.net/downloads/busybox-1.26.2.tar.bz2"
+#tar xvjf busybox-1.26.2.tar.bz2
+#wget "https://matt.ucc.asn.au/dropbear/releases/dropbear-2016.74.tar.bz2"
+#tar xvjf dropbear-2016.74.tar.bz2
 
-cp kernel/.config linux-4.11
-cp busybox/.config busybox-1.26.2
-cp dropbear/options.h dropbear-2016.74
+#cp kernel/.config linux-4.11
+#cp busybox/.config busybox-1.26.2
+#cp dropbear/options.h dropbear-2016.74
+
+mkdir artifacts
 
 cd linux-4.11
-make -j5
-
+#make -j5
+cp arch/x86_64/boot/bzImage ../artifacts
 cd ../busybox-1.26.2
-make -j5
+#make -j5
+cp busybox ../initrd/bin
 
 cd ../dropbear-2016.74
-./configure --disable-shadow --disable-lastlog --disable-syslog --disable-wtmp --disable-wtmpx --disable-utmpx
+#./configure --disable-shadow --disable-lastlog --disable-syslog --disable-wtmp --disable-wtmpx --disable-utmpx
 sed -i 's/22/22222/g' options.h
-make STATIC=1 MULTI=1
+#make STATIC=1 MULTI=1
+cp dropbearmulti ../initrd/bin
 
-mkdir ../artifacts
-
+cd ../sysinfo
+make
+cp sysinfo ../initrd/bin
 
 cd ../initrd
 find | cpio -L -v -o -H newc > ../artifacts/initrd.cpio
