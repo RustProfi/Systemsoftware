b3c0692447ba8a2f7a437f9a766bfea9f4dabb4c
Gabri312 <gabriel.cmiel@htwg-konstanz.de>
2018-12-08 15:18:23 +0100

    Gabri bester hacker EU

--- hw4/modules/openclose/openclose.c
+++ hw4/modules/openclose/openclose.c
@@ -10,48 +10,50 @@ static dev_t template_dev_number;
 static struct cdev * driver_object;
 struct class *template_class;
 static int write_count=0;
+int i;
 
 static int __init ModInit(void)
 {
     printk(KERN_ALERT "OpenClose init\n");
-    
-    if(alloc_chrdev_region(&template_dev_number,0,256,"OpenClose") < 0)
+    if(alloc_chrdev_region(&template_dev_number,0,257,"OpenClose")<0)
         return -EIO;
     driver_object = cdev_alloc(); /*Anmldeobjekt reservieren */
     if(driver_object==NULL)
         goto free_device_number;
     driver_object->owner = THIS_MODULE;
     driver_object->ops = &fops;
-    if(cdev_add(driver_object, template_dev_number, 1) < 0) //Treiber anmelden
+    if(cdev_add(driver_object, template_dev_number, 257) < 0) //Treiber anmelden
         goto free_cdev;
     /*Eintrag im Sysfs, damit Udev den Geräteeintrag erzeugt */
-    template_class = class_create(driver_object->owner, "chardrv");
-    if(template_class == NULL) {
-	class_destroy(template_class);
-	goto free_cdev;
+    template_class = class_create(driver_object->owner,"openclose");
+    for(i = 0;i < 257;i++){
+        if(device_create(template_class, NULL, MKDEV(template_dev_number,template_dev_number + i), NULL, "openclose%d", i) == NULL){
+            class_destroy(template_class);
+            unregister_chrdev_region(template_dev_number,257);
+        }
     }
-    device_create(template_class, NULL, template_dev_number, NULL, "%s", "simple_chardrv");
-    
     return 0;
+
     free_cdev:
     kobject_put(&driver_object->kobj );
     
     free_device_number:
-    unregister_chrdev_region(template_dev_number, 1);
+    unregister_chrdev_region(template_dev_number, 257);
     return -EIO;
 }
 
 static void __exit ModExit(void)
 {
     printk(KERN_ALERT "char dev kill\n");
-    device_destroy(template_class, template_dev_number);
+    for(i = 0; i < 257; i++){
+        device_destroy(template_class,MKDEV(template_dev_number,template_dev_number + i));
+    }
     class_destroy(template_class);
     /*Abmelden des Treibers*/
     cdev_del(driver_object);
     kobject_put(&driver_object->kobj);
-    unregister_chrdev_region(template_dev_number, 1);
+    unregister_chrdev_region(template_dev_number, 257);
     return;
-    
 }
 
 static int driver_open(struct inode *geraetedatei, struct file *instanz){
